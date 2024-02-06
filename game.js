1d8b6262fa3cf7f6bb3bf2c56dcb8b281a0dbbe2
Berk Belhan <berk.belhan@gmail.com>
2024-02-06 21:11:46 +0300

    Update game.js

--- game.js
+++ game.js
@@ -66,7 +66,7 @@ function nextSequence(){
 }
 
 function playSound(name){
-    var audio = new Audio('https://github.com/BerkBelhan/SimonGame/blob/main/' + name +'.mp3?raw=true');
+    var audio = new Audio("sounds/" + name + ".mp3");
     audio.play(); 
 }
 
